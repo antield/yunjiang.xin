b61caae5a1ae57864bae933cb2d014921c013457
Yunjiang Notebook <antield@126.com>
2025-04-21 21:56:10 +0800

    Updates

---
+++ js/compile-less/map_config.js
@@ -0,0 +1,12 @@
+var locationOrigin = "http://10.60.0.73:29996";
+// var locationOrigin = "http://10.60.0.44:29996";
+// var locationOrigin = "http://10.60.1.132:29996";
+// var locationOrigin = "http://10.60.0.44:29995";
+// var locationOrigin = location.origin;
+// var locationOrigin = "http://10.60.1.132:29995";
+
+var contextPath = "/zaxq-poi-api/"
+// var contextPath = "/poi-api/"
+// var contextPath = location.href.substring(locationOrigin.length,location.href.indexOf("/",locationOrigin.length+1)+1);
+
+var poiManageApiUrl = locationOrigin + contextPath;
