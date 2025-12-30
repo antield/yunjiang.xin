df0cbb5d9410600904619c33c87e57fb0f1ccc44
张运江 <dev_river@yeah.net>
2025-12-30 17:28:01 +0800

    loginSubmitContextPath

--- src/js/compile-less/map_config.js
+++ src/js/compile-less/map_config.js
@@ -1,1 +1,3 @@
-var loginSubmitContextPath = "http://10.60.0.44:16001/district-boundary-api/";
+
+// var loginSubmitContextPath = "http://10.60.0.44:16001/district-boundary-api/";
+var loginSubmitContextPath = "https://api.yunjiang.xin/web-api/";
